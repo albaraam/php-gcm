cdf3dae2549044e2489896b5249f5078888f1639
Albaraa Mishlawi <albaraa_m@live.com>
2015-12-08 16:44:12 +0200

    move exceptions each to its own file

---
+++ src/exceptions/AuthenticationException.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @author: Albaraa Mishlawi
+ * @package: albaraam/php-gcm
+ */
+
+namespace albaraam\gcm\exceptions;
+
+class AuthenticationException extends RuntimeException {}
---
+++ src/exceptions/TooBigPayloadException.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @author: Albaraa Mishlawi
+ * @package: albaraam/php-gcm
+ */
+
+namespace albaraam\gcm\exceptions;
+
+class TooBigPayloadException extends LogicException {}
